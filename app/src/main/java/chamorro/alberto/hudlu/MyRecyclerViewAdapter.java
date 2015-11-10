156850e1d5f7d7a87c7c439576482734ad8362e3
Alberto Chamorro <alberto.chamorro@hudl.com>
2015-11-10 18:28:19 +0000

    Populated the code of the RecyclerView adapter

--- app/src/main/java/chamorro/alberto/hudlu/MyRecyclerViewAdapter.java
+++ app/src/main/java/chamorro/alberto/hudlu/MyRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package chamorro.alberto.hudlu;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,9 +14,12 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     private String[] _dataSet;
 
     public static class ViewHolder extends  RecyclerView.ViewHolder {
-        
-        public ViewHolder(TextView itemView) {
+        TextView _myTextView;
+
+        public ViewHolder(View itemView) {
             super(itemView);
+
+            _myTextView = (TextView)itemView.findViewById(R.id.item_my_text);
         }
     }
 
@@ -25,17 +29,21 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_card_view, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder._myTextView.setText(_dataSet[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return _dataSet.length;
     }
 
 }
